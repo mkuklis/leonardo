9db78c178c6a1efb1b41d919c43ea86170968970
Michal Kuklis <michal.kuklis@gmail.com>
2012-04-20 02:29:00 -0400

    gruntify

--- grunt.js
+++ grunt.js
@@ -1,6 +1,7 @@
 config.init({
   min: {
-    'dist/leonardo.min.js': ['src/leonardo.js', 'src/color.js', 'src/matrix.js', 'src/element.js']
+    'dist/leonardo.min.js': ['src/leonardo.js', 'src/color.js',
+      'src/matrix.js', 'src/element.js', 'src/event.js', 'src/polyfill.js', 'src/easings.js', 'src/animation.js']
   },
   lint: {
     files: ['src/*.js']
