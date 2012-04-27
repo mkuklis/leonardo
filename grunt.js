b8592a173aef1b4c5e3fa4ce8260c8bf2c82fb29
Michal Kuklis <michal.kuklis@gmail.com>
2012-04-27 00:11:44 -0400

    added contact to grunt

--- grunt.js
+++ grunt.js
@@ -1,5 +1,23 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    concat: {
+      dist: {
+        src: [
+          'src/leonardo.js',
+          'src/color.js',
+          'src/matrix.js',
+          'src/transformation.js',
+          'src/element.js',
+          'src/emitter.js',
+          'src/event.js',
+          'src/polyfill.js',
+          'src/easings.js',
+          'src/animation.js'
+        ],
+        dest: 'leonardo.js'
+      }
+    },
+
     min: {
       dist: {
         src: [
@@ -33,5 +51,5 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('default', 'lint min');
+  grunt.registerTask('default', 'lint concat min');
 }
